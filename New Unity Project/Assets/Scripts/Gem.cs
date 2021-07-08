a50819e161bdbeddfb3d45a1ef5b4d5f9f1658a7
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-08 21:39:12 +0100

    collectable: rotating gem

---
+++ New Unity Project/Assets/Scripts/Gem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    public float speed = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0, Space.World); //rotate relative to the world
+    }
+}
