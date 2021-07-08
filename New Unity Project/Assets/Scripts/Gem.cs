f649173ff906fe1607de482f5bfcf8bc4ad54945
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-08 21:59:34 +0100

    pickup gem

--- New Unity Project/Assets/Scripts/Gem.cs
+++ New Unity Project/Assets/Scripts/Gem.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Gem : MonoBehaviour
 {
     public float speed = 0.3f;
+    public GameObject gem;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, speed, 0, Space.World); //rotate relative to the world
+    }
+
+    void OnTriggerEnter()
+    {
+        Destroy(gem);
     }
 }
