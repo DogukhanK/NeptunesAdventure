8cdf74afb0e8c9520e0956806a90552ae810908a
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-09 14:52:32 +0100

    skybox+direction light rotating for effect

--- New Unity Project/Assets/Scripts/SkyBox.cs
+++ New Unity Project/Assets/Scripts/SkyBox.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SkyBox : MonoBehaviour
 {
     public static float rotationSpeed = 0.8f;
+    public GameObject lightDir;
 
     void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+        lightDir.transform.Rotate(0, -0.0025f, 0, Space.World);
     }
 }
