f6341e6e00b95a081d68117e4cad8694908a6c56
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-09 20:12:21 +0100

    win text implemented

--- New Unity Project/Assets/Scripts/Finish.cs
+++ New Unity Project/Assets/Scripts/Finish.cs
@@ -6,7 +6,15 @@ public class Finish : MonoBehaviour
 {
     public GameObject music;
     public GameObject levelTimer;
+    public GameObject endTime;
+    public GameObject endTime2;
+    public GameObject endScore;
+    public GameObject endScore2;
+    public GameObject totalScore;
+    public GameObject totalScore2;
+
     public AudioSource levelWin;
+
     public static bool winCondition;
 
     void OnTriggerEnter()
@@ -15,5 +23,21 @@ public class Finish : MonoBehaviour
         levelTimer.SetActive(false);
         levelWin.Play();
         winCondition = true;
+
+        StartCoroutine(calculator());
+    }
+
+    IEnumerator calculator()
+    {
+        yield return new WaitForSeconds(0.5f);
+        endTime.SetActive(true);
+        endTime2.SetActive(true);
+        yield return new WaitForSeconds(0.75f);
+        endScore.SetActive(true);
+        endScore2.SetActive(true);
+        yield return new WaitForSeconds(1.25f);
+        totalScore.SetActive(true);
+        totalScore2.SetActive(true);
+
     }
 }
