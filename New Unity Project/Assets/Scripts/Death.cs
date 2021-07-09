a7f91189b22d8b25709c84b2d06a2d4000ba38f7
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-09 18:38:34 +0100

    death animation and audio

--- New Unity Project/Assets/Scripts/Death.cs
+++ New Unity Project/Assets/Scripts/Death.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
+    public GameObject death;
+    public GameObject music;
+    public GameObject fadeOut;
+    public AudioSource deathAudio;
 
     void OnTriggerEnter()
     {
+        StartCoroutine (died());
+    }
+
+    IEnumerator died()
+    {
+        death.SetActive(true);
+        music.SetActive(false);
+        deathAudio.Play();
+        yield return new WaitForSeconds(1);
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene(0);
     }
 }
