d2291968cd6c27c63ac39ea0487e7dca6e97ec62
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-09 14:14:30 +0100

    skybox rotation

---
+++ New Unity Project/Assets/Scripts/SkyBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBox : MonoBehaviour
+{
+    public static float rotationSpeed = 0.8f;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+    }
+}
