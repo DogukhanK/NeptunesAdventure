dcd28f4a78c6dbd71716e6ca18cdcc4da186ca42
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-10 18:16:19 +0100

    imported character model and animations // created script & animator which uses animations and audio to manipulate the model

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    public float speed = 5.0f;
+    public float rotationSpeed = 100.0f;
+
+    public AudioSource jumpAudio;
+    public AudioSource runAudio;
+    public AudioSource attackAudio;
+
+    private bool isRunning = false;
+    private bool jumpCooldown = false;
+    private bool attackCooldown = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Rotate(0, rotation, 0);
+
+        if (translation !=0)
+        {
+            anim.SetBool("isMoving", true);
+            isRunning = true;
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+
+        anim.SetFloat("MoveSpeed", translation);
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (attackCooldown == false)
+            {
+                anim.SetTrigger("Attack");
+                attackAudio.Play();
+                Invoke("ResetAttackCooldown", 0.9f);
+                attackCooldown = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCooldown == false)
+            {
+                anim.SetTrigger("Jump");
+                jumpAudio.Play();
+                Invoke("ResetJumpCooldown", 2.1f);   // add short delay which is slightly longer than the animation to prevent buttom spam
+                jumpCooldown = true;
+            }
+        }
+
+        if (translation==0)
+        {
+            isRunning = false;
+        }
+
+        if (isRunning)
+        {
+            if (!runAudio.isPlaying)
+            {
+                runAudio.Play();
+            }
+        }
+        else
+            runAudio.Stop();
+
+        if (jumpAudio.isPlaying)
+        {
+            runAudio.Stop();
+        }
+        if (attackAudio.isPlaying)
+        {
+            runAudio.Stop();
+        }
+    }
+
+    public void FootL()
+    {
+
+    }
+    public void FootR()
+    { 
+
+    }
+    public void Hit()
+    {
+
+    }
+
+    void ResetJumpCooldown()
+    {
+        jumpCooldown = false;
+    }
+    void ResetAttackCooldown()
+    {
+        attackCooldown = false;
+    }
+}
