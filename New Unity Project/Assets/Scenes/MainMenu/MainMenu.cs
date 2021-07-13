7ea4b31572e6e82cf5a80e74f1f6f0f3ec28dafd
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-13 20:40:20 +0100

    link between main menu -> level selection -> character selection -> level

--- New Unity Project/Assets/Scenes/MainMenu/MainMenu.cs
+++ New Unity Project/Assets/Scenes/MainMenu/MainMenu.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource click;
+
+    public void StartGame()
     {
-        
+        click.Play();
+        StartCoroutine(SceneDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Options()
     {
-        
+        click.Play();
+        StartCoroutine(OptionsDelay());
+    }
+
+    public void Quit()
+    {
+        click.Play();
+        StartCoroutine(QuitDelay());
+    }
+
+    IEnumerator SceneDelay()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator OptionsDelay()
+    {
+        yield return new WaitForSeconds(1);
+        //insert load options code
+    }
+
+    IEnumerator QuitDelay()
+    {
+        yield return new WaitForSeconds(1);
+        //insert quit code
     }
 }
