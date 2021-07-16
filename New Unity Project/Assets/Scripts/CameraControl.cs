661da569e7d77a058d991b43ad24cf0650915a5c
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-16 15:12:01 +0100

    cinemachine added

--- New Unity Project/Assets/Scripts/CameraControl.cs
+++ New Unity Project/Assets/Scripts/CameraControl.cs
@@ -6,15 +6,60 @@ public class CameraControl : MonoBehaviour
 {
     public Transform player;
     private Transform camera;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+    public float xSpeed = 250.0f;
+    public float ySpeed = 120.0f;
+
     public float camDist = 3.0f;
     public float camHeight = 2.0f;
+
+    private bool isKeyDown = false;
 
     void Start()
     {
         camera = transform;
+
+        cameraSetup();
     }
 
     void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isKeyDown = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isKeyDown = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (isKeyDown)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            y += Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            //
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -camDist) + player.position;
+
+            camera.rotation = rotation;
+            camera.position = position;
+        }
+        else
+        {
+            camera.position = new Vector3(player.position.x, player.position.y + camHeight, player.position.z - camDist);
+            x = 0;
+            y = 0;
+        }
+    }
+
+    public void cameraSetup()
     {
         camera.position = new Vector3(player.position.x, player.position.y + camHeight, player.position.z - camDist);
     }
