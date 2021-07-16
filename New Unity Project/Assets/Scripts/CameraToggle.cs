d17ef7347acfe4e367335293ff7354062fa326d1
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-16 15:33:21 +0100

    new third person camera

--- New Unity Project/Assets/Scripts/CameraToggle.cs
+++ New Unity Project/Assets/Scripts/CameraToggle.cs
@@ -6,16 +6,20 @@ public class CameraToggle : MonoBehaviour
 {
     public GameObject camera;
     public GameObject camera2;
+    public GameObject cinemachine;
+    public GameObject cinemachine2;
 
     void Start()
     {
         if (CharacterSelection.index == 1)
         {
             camera.SetActive(false);
+            cinemachine.SetActive(false);
         }
         if (CharacterSelection.index == 0)
         {
             camera2.SetActive(false);
+            cinemachine2.SetActive(false);
         }
     }
 }
