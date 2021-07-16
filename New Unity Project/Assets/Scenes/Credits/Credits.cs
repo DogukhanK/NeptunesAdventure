4b4b48ae7dc140e6805fb108eada0fc3a2227174
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-16 14:32:46 +0100

    credits

---
+++ New Unity Project/Assets/Scenes/Credits/Credits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(creditDelay());
+    }
+
+    IEnumerator creditDelay()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(0);
+    }
+}
