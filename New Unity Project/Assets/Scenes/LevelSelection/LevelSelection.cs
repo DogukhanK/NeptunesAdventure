e9e00e22601ee5862d0c323ca5c9ddd985a0917d
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-17 14:46:48 +0100

    imported environment prefabs

--- New Unity Project/Assets/Scenes/LevelSelection/LevelSelection.cs
+++ New Unity Project/Assets/Scenes/LevelSelection/LevelSelection.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelSelection : MonoBehaviour
 {
     public static int currentLevelCode;
-    public static int isCompleted = 0;
+    public static int isCompleted = 1;
 
     public GameObject Level2Button;
     public GameObject Level3Button;
