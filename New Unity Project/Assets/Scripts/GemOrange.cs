8b9285554fb7349afcbeec338f898b17d0a96037
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-17 14:20:10 +0100

    third gem with different value

---
+++ New Unity Project/Assets/Scripts/GemOrange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemOrange : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public AudioSource sfx;
+    public GameObject score;
+    public GameObject score2;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.timeScale, 0, Space.World);  //rotate relative to the world
+    }
+
+    void OnTriggerEnter()
+    {
+        UpdateScore.currentScore += 30;               //update score value in UpdateScore script
+        UpdateScore.gemsCollected += 1;              //update total gems collected 
+        sfx.Play();                                  //play sfx 
+        Destroy(gameObject);                         //remove gem form the world
+    }
+}
