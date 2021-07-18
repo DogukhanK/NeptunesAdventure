6180a337bb9de16be3fe96d9a773f9ab3627bfdd
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-18 16:17:53 +0100

    npc dialogue

---
+++ New Unity Project/Assets/Scenes/Level 2/DialogueTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject talkPrompt;
+    public GameObject textBox;
+
+    public NPCDialogue dialogue;
+
+    void OnTriggerEnter()
+    {
+        talkPrompt.SetActive(true);
+    }
+
+    void OnTriggerStay()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Cursor.visible = true;
+            Time.timeScale = 0;
+            textBox.SetActive(true);
+            triggerDialogue();
+        }
+    }
+
+    public void triggerDialogue()
+    {
+        talkPrompt.SetActive(false);
+        FindObjectOfType<NPC>().startConversation(dialogue);
+    }
+}
--- New Unity Project/Assets/Scenes/Level 2/NPC.cs
+++ New Unity Project/Assets/Scenes/Level 2/NPC.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Queue<string> dialogueText;
+
+    public Text name;
+    public Text uiDialogue;
+
     void Start()
     {
-        
+        dialogueText = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void startConversation(NPCDialogue dialogue)
     {
-        
+        name.text = dialogue.name;
+
+        dialogueText.Clear();
+
+        foreach (string sentence in dialogue.dialogueText)
+        {
+            dialogueText.Enqueue(sentence);
+        }
+
+        DisplayDialogue();
+    }
+
+    public void DisplayDialogue()
+    {
+        if (dialogueText.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = dialogueText.Dequeue();
+        uiDialogue.text = sentence;
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("end");
     }
 }
+
