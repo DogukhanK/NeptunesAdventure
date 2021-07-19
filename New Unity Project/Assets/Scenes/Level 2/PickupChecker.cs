7996da273eb296f89007c8e6f3608d10c8b51321
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-19 19:22:21 +0100

    level 2 gems added

---
+++ New Unity Project/Assets/Scenes/Level 2/PickupChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupChecker : MonoBehaviour
+{
+    public GameObject tile;
+    public GameObject tile2;
+    public GameObject tile3;
+    public GameObject tile4;
+
+    public GameObject gem1;
+    public GameObject gem2;
+    public GameObject gem3;
+    public GameObject gem4;
+
+    void Update()
+    {
+        isPickedUp();
+    }
+
+    public void isPickedUp()
+    {
+        if (gem1 == null)
+        {
+            tile.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (gem2 == null)
+        {
+            tile2.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (gem3 == null)
+        {
+            tile3.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (gem4 == null)
+        {
+            tile4.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+}
