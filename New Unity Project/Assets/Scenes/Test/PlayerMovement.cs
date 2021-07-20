155a84573a724bdad96b12769a3fd28d208127fd
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-20 15:00:49 +0100

    test scene to make new character

---
+++ New Unity Project/Assets/Scenes/Test/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+
+    private CharacterController controller;
+}
