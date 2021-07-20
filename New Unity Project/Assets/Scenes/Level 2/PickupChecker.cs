afc844379f45c6ba4e9e1101f796b13a2ae7e207
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-20 14:49:09 +0100

    gem spawn when previous is destroyed. when all gems are collected, door opens

--- New Unity Project/Assets/Scenes/Level 2/PickupChecker.cs
+++ New Unity Project/Assets/Scenes/Level 2/PickupChecker.cs
@@ -16,9 +16,29 @@ public class PickupChecker : MonoBehaviour
 
     public static int totalCollected = 0;
 
+    public bool phase1 = true;
+    public bool phase2 = false;
+    public bool phase3 = false;
+    public bool phase4 = false;
+
     void Update()
     {
-        isPickedUp();
+        if (phase1)
+        {
+            isPickedUp();
+        }
+        if (phase2)
+        {
+            isPickedUp2();
+        }
+        if (phase3)
+        {
+            isPickedUp3();
+        }
+        if (phase4)
+        {
+            isPickedUp4();
+        }
     }
 
     public void isPickedUp()
@@ -26,26 +46,51 @@ public class PickupChecker : MonoBehaviour
         if (gem1 == null)
         {
             tile.GetComponent<Renderer>().material.color = Color.green;
-            totalCollected++;
-        }
-
-        if (gem2 == null)
-        {
-            tile2.GetComponent<Renderer>().material.color = Color.green;
-            totalCollected++;
-        }
-
-        if (gem3 == null)
-        {
-            tile3.GetComponent<Renderer>().material.color = Color.green;
-            totalCollected++;
-        }
-
-        if (gem4 == null)
-        {
-            tile4.GetComponent<Renderer>().material.color = Color.green;
-            totalCollected++;
+            totalCollected = 1;
+            gem2.SetActive(true);
+            phase1 = false;
+            phase2 = true;
         }
     }
 
+    public void isPickedUp2()
+    {
+        if (gem2 == null)
+        {
+            tile2.GetComponent<Renderer>().material.color = Color.green;
+            totalCollected = 2;
+            gem3.SetActive(true);
+            phase2 = false;
+            phase3 = true;
+        }
+    }
+
+    public void isPickedUp3()
+    {
+        if (gem3 == null)
+        {
+            tile3.GetComponent<Renderer>().material.color = Color.green;
+            totalCollected = 3;
+            gem4.SetActive(true);
+            phase3 = false;
+            phase4 = true;
+        }
+    }
+
+    public void isPickedUp4()
+    {
+        if (gem4 == null)
+        {
+            tile4.GetComponent<Renderer>().material.color = Color.green;
+            totalCollected = 4;
+            phase4 = false;
+            StartCoroutine(count());
+        }
+    }
+
+    IEnumerator count()
+    {
+        yield return new WaitForSeconds(1);
+        totalCollected = 5;
+    }
 }
