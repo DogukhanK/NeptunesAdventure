146a2e76149bc3e75f33764d377769f62492c60a
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-20 17:30:37 +0100

    character 1 movement fixed

---
+++ New Unity Project/Assets/Characters/NEPTUNENEW/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float mouseSens;
+
+    private Transform parent;
+
+    void Start()
+    {
+        parent = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+
+        parent.Rotate(Vector3.up, mouseX);
+    }
+}
