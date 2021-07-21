d17febd7ab0117a65b3cde36a16f1c3f6db4c94b
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-21 18:39:01 +0100

    options menu with volume sliders

---
+++ New Unity Project/Assets/Scenes/MainMenu/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+    public AudioMixer sfxMixer;
+
+    public void SetMasterVolume(float masterVolume)
+    {
+        masterMixer.SetFloat("volume", masterVolume);
+    }
+
+    public void SetSFXVolume(float sfxVolume)
+    {
+        sfxMixer.SetFloat("volume", sfxVolume);
+    }
+}
