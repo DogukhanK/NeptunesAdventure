538c48bb19a35dba7aa391f70b2ed7ffe40a9fa2
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-24 16:28:51 +0100

    update

---
+++ New Unity Project/Assets/Scripts/Obstacles/Rotating.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+
+    private float speed = 7.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed * 10);
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("ok");
+    }
+}
