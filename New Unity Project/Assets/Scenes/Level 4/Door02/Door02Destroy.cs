3e5243caa83f31d31759a2aa5543463d9732879a
Dkhan2000 <30826583+Dkhan2000@users.noreply.github.com>
2021-07-27 17:17:47 +0100

    deleted unused assets + imported assets for level 3 design + added level 4 map

---
+++ New Unity Project/Assets/Scenes/Level 4/Door02/Door02Destroy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door02Destroy : MonoBehaviour
+{
+    public GameObject door02;
+
+    public GameObject smoke;
+
+    void Start()
+    {
+        door02.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void Update()
+    {
+        if (UpdateScore.gemsCollected == 11)
+        {
+            door02.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (other.tag == "Player1")
+            {
+                if (UpdateScore.gemsCollected==11)
+                {
+                    Destroy(door02);
+                    GameObject effect = (GameObject)Instantiate(smoke, transform.position, Quaternion.identity);
+                    Destroy(effect, 3.0f);
+                }
+            }
+        }
+    }
+}
